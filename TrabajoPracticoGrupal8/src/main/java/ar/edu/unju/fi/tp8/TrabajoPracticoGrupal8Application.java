9a607c795df99bf9235138f00cd2f9b909887564
Paola <Paola@DESKTOP-U6UR9H7>
2021-06-02 22:05:55 -0300

    Paola Flores:
    Primer commit del trabajo practico N°8 del grupo EJPK21.

---
+++ TrabajoPracticoGrupal8/src/main/java/ar/edu/unju/fi/tp8/TrabajoPracticoGrupal8Application.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.tp8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoPracticoGrupal8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoPracticoGrupal8Application.class, args);
+		//GRUPO: EJPK21	
+		//Karen Daiana Flores - USUARIO: KarenDay
+		//Paola Fernanda Flores - USUARIO: pao15ELF
+		//Ezequiel Estrada - USUSARIO:
+		//Juan Baysse - USUARIO:
+	}
+
+}
